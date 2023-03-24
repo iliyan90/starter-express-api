289f7fcef38336e265a937fe70d852bd067b144c
iliyan90 <74927065+iliyan90@users.noreply.github.com>
2023-03-24 19:22:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,41 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+import express from 'express'
+const app = express();
+import http from 'http'
+const server = http.createServer(app);
+import cors from 'cors'
+import { Server } from 'socket.io';
+
+
+const io = new Server(server, {
+    cors: '*',
+    method: ["GET", "POST"]
+});
+const corsOptions = {
+    origin: 'http://videochat77.rf.gd',
+    credentials: true
+  }
+app.use(cors(corsOptions));
+
+const PORT = 3001;
+
+app.get('/',(req, res) => {
+    res.send('Server is running')
 })
-app.listen(process.env.PORT || 3000)
+
+io.on('connection', (socket) =>{
+    socket.emit('me', socket.id)
+    socket.on('disconnect', () =>{
+        socket.broadcast.emit('callended')
+    });
+    socket.on('calluser', ({userToCall, signalData, from, name}) =>{
+        io.to(userToCall).emit('calluser', {signal: signalData, from, name})
+    })
+    socket.on('answercall', (data) =>{
+        io.to(data.to).emit('callaccepted', data.signal);
+    })
+})
+
+server.listen(PORT, () =>{
+    console.log('connected to: ', PORT);
+})
+
